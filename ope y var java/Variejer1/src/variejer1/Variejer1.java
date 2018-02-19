765680f123af34776dd36fe978e0aa0f0c7aa23d
Dairo Estiben Beltrán <daiydan99@hotmail.com>
2018-02-19 15:28:48 -0500

    ejercicio 1 (variables y operadores)
    variables y operadores java

---
+++ ope y var java/Variejer1/src/variejer1/Variejer1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variejer1;
+
+/**
+ *
+ * @author estiben beltran
+ */
+class Estudiantes{
+    int estudiantes = 240;
+    int menores = 35;
+    float por;
+}
+public class Variejer1 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Estudiantes estudi = new Estudiantes();
+        estudi.por = (estudi.menores * 100)/ estudi.estudiantes;
+        System.out.println("La cantidad de estudinates en el salon 301 es "+estudi.estudiantes);
+        System.out.println("la cantidad de menores de edad en el salon 301 es "+estudi.menores);
+        System.out.println("El porcentaje de menores de edad en el salon 301 es "+estudi.por);
+        
+    }
+    
+}
