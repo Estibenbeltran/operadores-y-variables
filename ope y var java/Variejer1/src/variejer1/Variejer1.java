3f5080757a5095f770cde050f3d1b18a3d134718
Dairo Estiben Beltrán <daiydan99@hotmail.com>
2018-02-19 15:44:56 -0500

    variables y operadores
    finalizado, comentado y operado

--- ope y var java/Variejer1/src/variejer1/Variejer1.java
+++ ope y var java/Variejer1/src/variejer1/Variejer1.java
@@ -13,22 +13,28 @@ package variejer1;
  * @author Dairo Estiben Beltran Martinez
  * 
  */
-// 
+/**
+ * clase que contiene los atributos
+ */
 class Estudiantes{
-    int estudiantes = 240;
-    int menores = 35;
-    float por;
+    int estudiantes = 240;//cantidad de estudiantes
+    int menores = 35;//cantidad de estudiantes menores de edad
+    float por;//porcentaje de estudiantes
 }
+/**
+ * 
+ * clase principal
+ */
 public class Variejer1 {
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Estudiantes estudi = new Estudiantes();
-        estudi.por = (estudi.menores * 100)/ estudi.estudiantes;
-        System.out.println("La cantidad de estudinates en el salon 301 es "+estudi.estudiantes);
-        System.out.println("la cantidad de menores de edad en el salon 301 es "+estudi.menores);
-        System.out.println("El porcentaje de menores de edad en el salon 301 es "+estudi.por);
+        Estudiantes estudi = new Estudiantes();//declaracion del objeto estudiante
+        estudi.por = (estudi.menores * 100)/ estudi.estudiantes;//operacion para saber el pporcentaje de estudiantes menores de edad
+        System.out.println("La cantidad de estudinates en el salon 301 es "+estudi.estudiantes);//muestra de datos
+        System.out.println("la cantidad de menores de edad en el salon 301 es "+estudi.menores);//muestra de datos
+        System.out.println("El porcentaje de menores de edad en el salon 301 es "+estudi.por);//muestra de datos
         
     }
     
